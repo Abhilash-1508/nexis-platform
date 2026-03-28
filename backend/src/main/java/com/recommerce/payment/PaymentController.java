4588b66f3928454e976d3e026246572f0077abef
Abhilash-1508 <eabhilash293@gmail.com>
2026-03-29 03:11:46 +0530

    Fix: Resolve build errors and add Dockerfile

--- backend/src/main/java/com/recommerce/payment/PaymentController.java
+++ backend/src/main/java/com/recommerce/payment/PaymentController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.recommerce.product.OrderRepository;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.PaymentIntent;
